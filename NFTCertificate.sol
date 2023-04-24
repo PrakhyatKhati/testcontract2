2417dd54357377811d1b8523d70fac736cbc427c
Prakhyat Khati <90866319+PrakhyatKhati@users.noreply.github.com>
2023-04-23 19:41:42 -0600

    Update NFTCertificate.sol

--- NFTCertificate.sol
+++ NFTCertificate.sol
@@ -1,7 +1,9 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NFTCertificate is ERC721URIStorage {
 
