f62d2f9a88325b02e2abaa3ff2feea7b4ea6941e
Prakhyat Khati <90866319+PrakhyatKhati@users.noreply.github.com>
2023-04-23 19:40:30 -0600

    Update NFTCertificate.sol

--- NFTCertificate.sol
+++ NFTCertificate.sol
@@ -2,9 +2,6 @@
 pragma solidity ^0.8.0;
 
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
 
 contract NFTCertificate is ERC721URIStorage {
 
